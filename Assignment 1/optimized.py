e67da67c7d414332e5b41ef1fd19efeeddc03acd
Derik <62659313+makemepresident@users.noreply.github.com>
2020-11-10 18:03:46 -0800

    minor bugfixes for re-hand-in

--- Assignment 1/optimized.py
+++ Assignment 1/optimized.py
@@ -8,19 +8,23 @@ candidate = []
 level_union = []
 level_union_count = []
 dcl = []
+current_level = 2
 
 filepath = None
 
 # Implemented from included wiki_algo screenshot
 # https://en.wikipedia.org/wiki/Apriori_algorithm
 def apriori():
+    global level
     global minsup
     global candidate
     global level_union
+    global current_level
     candidate = init_candidate() # Initial candidate table containing singletons
     while len(candidate) != 0:
         gen_level_table()
         gen_candidate()
+        current_level += 1
 
 def parseEle(string):
     parsed = string.split('\t')
@@ -47,6 +51,7 @@ def init_candidate():
         for i in elements:
             if {i} not in unique and i != '':
                 unique.append({i})
+    db.close()
     return unique
 
 def gen_level_table():
@@ -68,12 +73,14 @@ def gen_level_table():
                     break
             if flag:
                 level[index2] += 1
+    db.close()
 
 def gen_candidate():
     global db_len
     global candidate
     global dcl
     global level_union
+    global current_level
     min_count = db_len * minsup
     temp_candidate = []
     for index, item in enumerate(candidate):
@@ -91,6 +98,8 @@ def gen_candidate():
             for neg in dcl:
                 if neg.issubset(x):
                     continue
+            if len(x) != current_level:
+                continue
             if x not in temp_candidate:
                 temp_candidate.append(x)
     candidate = temp_candidate
@@ -107,7 +116,7 @@ def main():
         filepath = sys.path[0] + "\\" + sys.argv[1]
     else:
         x = float(sys.argv[2])
-        if x > 1:
+        if x >= 1:
             x /= 100
         filepath = sys.path[0] + "\\" + sys.argv[1]
         minsup = x
